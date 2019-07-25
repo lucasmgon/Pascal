7e39927391ede689e8e7838edb714dc9f8ad1a1c
LucasMGon <lucas.66matheus@gmail.com>
2019-07-25 19:49:22 -0400

    Exemplo de sqrt (Raiz Quadrada)

---
+++ Pascal/Aula 02/02-Exemplo_sqrt.pas
@@ -0,0 +1,13 @@
+//Exemplo de uso do sqrt para calcular a raiz quadrada | 2015
+program raiz_quadrada;
+
+var resultado1, resultado2: real;
+
+begin
+	resultado1:= sqrt(9);
+	resultado2:= sqrt(81);
+	
+	writeln('A raiz quadrada de 9 e: ',resultado1 :2:2);
+	writeln('A raiz quadrada de 81 e: ',resultado2 :2:2);
+	readln;
+end.
