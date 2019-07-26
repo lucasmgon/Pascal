74542880509eea914d20f22907a3bf7ce4c4edf5
LucasMGon <lucas.66matheus@gmail.com>
2019-07-25 20:28:55 -0400

    Tabuada do 10

---
+++ Pascal/Aula 03/01-Exercicio_1.pas
@@ -0,0 +1,20 @@
+//Faça um algoritmo que exiba a tabuada do 10
+program tabuada;
+
+var n1: real;
+
+begin
+	n1:= 10;
+	
+	writeln('01 x ',n1 :2:2,' = ',n1*1 :2:2);
+	writeln('02 x ',n1 :2:2,' = ',n1*2 :2:2);
+	writeln('03 x ',n1 :2:2,' = ',n1*3 :2:2);
+	writeln('04 x ',n1 :2:2,' = ',n1*4 :2:2);
+	writeln('05 x ',n1 :2:2,' = ',n1*5 :2:2);
+	writeln('06 x ',n1 :2:2,' = ',n1*6 :2:2);
+	writeln('07 x ',n1 :2:2,' = ',n1*7 :2:2);
+	writeln('08 x ',n1 :2:2,' = ',n1*8 :2:2);
+	writeln('09 x ',n1 :2:2,' = ',n1*9 :2:2);
+	writeln('10 x ',n1 :2:2,' = ',n1*10 :2:2);
+	readln;
+end.
